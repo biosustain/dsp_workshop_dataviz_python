5edcb10b2cdfc262bb925dbde052c00f3848743f
Henry Webel <heweb@dtu.dk>
2025-11-11 15:58:48 +0100

    🚧 load data directly from dictionary (for portability)

--- exercises/CRISPRi/figure_2a.py
+++ exercises/CRISPRi/figure_2a.py
@@ -12,12 +12,28 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
-# %%
-fname = "figure_2a.csv"
+# %% [markdown]
+# For protability the data is directly included as a dictionary. See the commented
+# out code how the data was obtained from the CSV file [figure_2a.csv](figure_2a.csv).
 
 # %%
-data = pd.read_csv(fname, index_col=0).rename_axis(columns="replicate")
-data.head()
+# fname = "figure_2a.csv"
+# data = pd.read_csv(fname)
+# data.to_dict(orient="list")
+data = {
+    "time (h)": [0, 2, 4, 6, 8, 12, 16, 24, 30],
+    "rep1_uninduced": [0.046, 0.194, 0.74, 1.64, 2.62, 3.68, 4.08, 4.17, 3.88],
+    "rep2_uninduced": [0.048, 0.213, 0.82, 1.8, 2.72, 3.73, 4.32, 4.13, 3.87],
+    "rep3_uninduced": [0.06, 0.234, 0.85, 1.91, 2.81, 3.92, 4.36, 4.23, 3.95],
+    "rep4_uninduced": [0.054, 0.205, 0.84, 1.72, 2.7, 3.88, 4.24, 4.18, 4.01],
+    "rep1_induced": [0.046, 0.185, 0.5, 0.76, 0.97, 1.01, 0.93, 0.97, 0.9],
+    "rep2_induced": [0.055, 0.184, 0.493, 0.766, 0.99, 0.94, 0.97, 1.03, 0.94],
+    "rep3_induced": [0.05, 0.204, 0.534, 0.815, 1.06, 1.15, 1.06, 1.07, 1.01],
+    "rep4_induced": [0.046, 0.192, 0.537, 0.825, 1.1, 1.17, 1.16, 1.13, 0.97],
+}
+data = pd.DataFrame(data).set_index("time (h)").rename_axis(columns="replicate")
+# data.head()
+
 
 # %%
 data_long = (
@@ -56,7 +72,6 @@ ax.legend(title="Condition")
 fig.tight_layout()
 
 
-# %%
 # %% [markdown]
 # <details>
 # <summary>Show one solution code</summary>
@@ -87,5 +102,3 @@ fig.tight_layout()
 # ```
 #
 # </details>
-
-# %%
