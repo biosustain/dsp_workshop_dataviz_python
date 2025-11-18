110a511b7d50cba01d7bb541fb19962eabd32b14
Henry Webel <heweb@dtu.dk>
2025-11-18 16:03:41 +0000

    :wrench: setup codespace for static image export from plotly

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,3 @@
+sudo apt update && sudo apt-get -y install libnss3 libatk-bridge2.0-0 libcups2 libxcomposite1 libxdamage1 libxfixes3 libxrandr2 libgbm1 libxkbcommon0 libpango-1.0-0 libcairo2 libasound2
+pip3 install --user -r requirements.txt
+kaleido_get_chrome
